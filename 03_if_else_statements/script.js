5c88007950d635d5b5a0b716d5afa254d1510333
Jonnie <jonnie.muis@gmail.com>
2020-11-02 09:05:52 +0100

    Add files via upload

---
+++ 03_if_else_statements/script.js
@@ -0,0 +1,23 @@
+const age = 37;
+
+if (age >= 18) {
+    console.log('Come in and have fun!');
+} else {
+    console.log('Sorry, you\'re too young to come in');
+}
+
+const isFemale = false;
+
+if (isFemale) {
+    console.log('Have fun at Ladiesnight');
+} else {
+    console.log('You\'re not a woman, so you can\'t come in at Ladiesnight!')
+}
+
+const driverStatus = 'bob';
+
+if (driverStatus == 'bob') {
+    console.log('You can drive home');
+} else {
+    console.log('You\'re not allowed to drive home');
+}
